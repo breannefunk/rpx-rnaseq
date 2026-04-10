421d0812a0e90288bdb00ca598dee9d4d25f9213
Breanne Funk <bgf57696@uga.edu>
2026-04-10 18:18:12 -0400

    changed outlog directory to dedicated outlog directory within fastqc_results
    removed "cd $OUTDIR"
    
    added FASTQ_LIST variable for full path to fastq_list.txt, replaced relative path "fastq_list.txt" in FASTQC variable to full path via $FASTQ_LIST

--- fastqc_results/fastqc_array.sh
+++ fastqc_results/fastqc_array.sh
@@ -6,19 +6,19 @@
 #SBATCH --mem=8G                                    # Memory per node (24GB); by default using M as unit
 #SBATCH --time=1:00:00                              # Time limit hrs:mins:secs or days-hrs:mins:secs
 #SBATCH --array=1-271%30
-#SBATCH --output=/work/gene8940/bgf57696/rpx-rnaseq/fastqc_results/%x_%j.out  # Standard output log, e.g., testBowtie2_12345.out
+#SBATCH --output=/work/gene8940/bgf57696/rpx-rnaseq/fastqc_results/outlogs/%x_%j.out  # Standard output log, e.g., testBowtie2_12345.out
 #SBATCH --mail-user=bgf57696@uga.edu                # Where to send mail
 #SBATCH --mail-type=ALL                        # Mail events (BEGIN, END, FAIL, ALL)
 
 #set outdir & fastQC variables
 OUTDIR=/work/gene8940/bgf57696/rpx-rnaseq/fastqc_results/
-cd $OUTDIR
 
-FASTQC=$(sed -n "${SLURM_ARRAY_TASK_ID}p" fastq_list.txt)
+FASTQ_LIST=/work/gene8940/bgf57696/rpx-rnaseq/fastqc_results/fastq_list.txt
+
+FASTQC=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$FASTQ_LIST")
 
 #load FastQC
 ml FastQC/0.12.1-Java-11
 
 #run FastQC
 fastqc -t 4 -o "$OUTDIR" "$FASTQC"
-
