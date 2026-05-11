8bce5e84e17dd6608210a2ac032915b618770af2
Breanne Funk <bgf57696@uga.edu>
2026-05-11 00:49:06 -0400

    upload

---
+++ fastqc_results/multiqc/multiqc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=multiqc.sh                      # Job name
+#SBATCH --partition=batch                           # Partition name (batch, highmem, or gpu)
+#SBATCH --ntasks=1                                  # 1 task (process) for below commands
+#SBATCH --cpus-per-task=4                           # CPU core count per task, by default 1
+#SBATCH --mem=8G                                    # Memory per node (24GB); by default using M as unit
+#SBATCH --time=1:00:00                              # Time limit hrs:mins:secs or days-hrs:mins:secs
+#SBATCH --array=1-271%30
+#SBATCH --output=/work/gene8940/bgf57696/rpx-rnaseq/fastqc_results/outlogs/%x_%j.out  # Standard output log, e.g., testBowtie2_12345.out
+#SBATCH --mail-user=bgf57696@uga.edu                # Where to send mail
+#SBATCH --mail-type=ALL                        # Mail events (BEGIN, END, FAIL, ALL)
+
+#load MultiQC module
+ml MultiQC/1.28-foss-2024a
+
+#set output variable
+OUTDIR=/work/gene8940/bgf57696/rpx-rnaseq/fastqc_results/multiqc/
+
+multiqc --file-list fastq_list.txt --outdir .
+
